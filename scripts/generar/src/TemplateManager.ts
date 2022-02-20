7548b34bc81e3ac7134e70d6a731c45e007b10dc
JC Olivares <juancri@juancri.com>
2022-02-20 01:04:32 -0600

    Renombrando templates/constitucion.md a templates/constitucion.txt para permitir la visualización correcta en GitHub

--- scripts/generar/src/TemplateManager.ts
+++ scripts/generar/src/TemplateManager.ts
@@ -6,7 +6,7 @@ import Handlebars from 'handlebars';
 import { TemplateData } from './types';
 
 // Constants
-const TEMPLATE_PATH = path.join(__dirname, '../templates/constitucion.md');
+const TEMPLATE_PATH = path.join(__dirname, '../templates/constitucion.txt');
 const TEMPLATE_STRING = fs.readFileSync(TEMPLATE_PATH).toString();
 const TEMPLATE = Handlebars.compile(TEMPLATE_STRING);
 const OUTPUT_PATH = path.join(__dirname, '../output/constitucion.md');
